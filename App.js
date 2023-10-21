89ae9bfc5ecb3ae8e04c267d640dd4f40ba4cd67
amit <ak910221@gmail.com>
2023-10-21 23:09:47 +0530

    episode 1 21 oct

--- App.js
+++ App.js
@@ -1,3 +1,24 @@
-const heading = React.createElement("h1",{},"Hello World from React!");
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const parent = React.createElement("div",{id: "parent"},[
+    React.createElement("div",{id:"child"},[
+        React.createElement("h1",{},"This is Namaste React "),
+        React.createElement("h2",{},"by Akshay Saini"),
+
+    ]),
+    React.createElement("div",{id:"child2"},[
+        React.createElement("h1",{},"I'm an h1 tag"),
+        React.createElement("h2",{},"I'm an h2 tag"),
+    ]),
+]);
+console.log(parent);//object
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(parent);
+
+// the above code is so complex so to resolve it we use JSX to make our life easy
+//but the core of react is above thing jsx makes our life easy behind the seen JSX convert code in above form
+
+//This is the last time we are using
+//React.createElement bye bye this now we use JSX for react element
+
--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # Namaste React
+
+# Parcel
+- Dev Build
+- Local Server
+- HMR = Hot Module Replacement
+- File Watching Algorithm - written in C++
+- Caching - Faster Builds
+- Image Optimization
+- Minification of file
+- Bundling
+- Compressing our file
+- Consistent Hashing
+- Code Splitting
+- Differential Bundling -- our app can run older browser as well (means support older browser)
+- Diagnostic
+- Error Handling
+- HTTPs
+- Tree Shaking - remove unused code 
+- Different dev and prod bundle
+
+
+
